62e98c2f0fcb1c7cf922555a34951061d6f812ec
Charles Villard <c.d.villard@gmail.com>
2020-02-08 18:10:56 -0500

    Step 2 checkpoint

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  plugins: ['gatsby-plugin-emotion'],
+  siteMetadata: {
+    title: 'Gatsby as a Service',
+    description: 'My coursework learning Gatsby from Jason Lengstory on FEM',
+  },
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.js'),
+        },
+      },
+    },
+  ],
 };
