315f811b64dbdca3f417c968c7a98c2b91a78957
Charles Villard <c.d.villard@gmail.com>
2020-02-10 12:07:58 -0500

    Step 7 checkpoint

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
-export default () => (
-  <Layout>
-    <h1>Hello Gatsby! Let's build a SaaS!</h1>
-    <p>Let's try and level up, shall we?</p>
-    <Link to="/about">Learn about me!</Link>
-  </Layout>
-);
+import usePosts from '../hooks/use-posts';
+import PostPreview from '../components/post-preview';
+import Hero from '../components/hero';
+import Insta from '../components/insta';
+
+export default () => {
+  const posts = usePosts();
+
+  return (
+    <>
+      <Hero />
+      <Layout>
+        <h2>Posts</h2>
+        {posts.map(post => (
+          <PostPreview key={post.slug} post={post} />
+        ))}
+
+        <Insta />
+      </Layout>
+    </>
+  );
+};
