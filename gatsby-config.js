e15e7a4ac75395042cb418edfc64e01b12aff056
Charles Villard <c.d.villard@gmail.com>
2020-02-11 01:42:28 -0500

    Netlify hosting checkpoint

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,5 +35,14 @@ module.exports = {
       resolve: 'gatsby-source-instagram',
       options: { username: 'gatsbyjs' },
     },
+    {
+      resolve: 'gatsby-plugin-webpack-bundle-analyzer',
+      options: {
+        production: true,
+        disable: !process.env.ANALYZE_BUNDLE_SIZE,
+        generateStatsFile: true,
+        analyzerMode: 'static',
+      },
+    },
   ],
 };
